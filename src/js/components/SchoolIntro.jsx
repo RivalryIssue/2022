db326f4781e845dc069509d354b781934b6caf2b
erxclau <ericlau41802@gmail.com>
2022-10-10 18:26:13 -0400

    Ignore eslint error for allowFullScreen on iframe
    The YouTube player seems to need allowFullScreen in order for the
    fullscreen button to be enabled.

--- src/js/components/SchoolIntro.jsx
+++ src/js/components/SchoolIntro.jsx
@@ -5,15 +5,20 @@ const SchoolIntro = ({ tab }) => (
   <div className="container" style={{ marginTop: "20px" }}>
     <div className="schoolIntro">
       <div className="player-wrapper">
-        <iframe 
+        <iframe
           loading="lazy"
-          width="100%" 
-          height="100%" 
-          src={tab === "UM" ? "https://www.youtube.com/embed/oIs_KnbBD7g" : "https://www.youtube.com/embed/VTEqUF289pk" }
-          title="YouTube video player" 
-          frameBorder="0" 
-          allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" 
-          allowFullScreen 
+          width="100%"
+          height="100%"
+          src={
+            tab === "UM"
+              ? "https://www.youtube.com/embed/oIs_KnbBD7g"
+              : "https://www.youtube.com/embed/VTEqUF289pk"
+          }
+          title="YouTube video player"
+          frameBorder="0"
+          allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+          // eslint-disable-next-line react/no-unknown-property
+          allowFullScreen
         />
       </div>
     </div>
